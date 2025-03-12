a03d9ce8d768e86009d8e6568bb518f816d415f9
Lucasvpl <lucasvpldeveloper@gmail.com>
2025-03-12 00:53:28 -0300

    Aula 1 e 2 hora de codar

---
+++ 2_FUNDAMENTOS/2_FUNDAMENTOS/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import FirstComponent from './components/FirstComponenet'
+
+function App() {
+  return (
+    <>
+      <h1>Fundamentos do React</h1>
+      <FirstComponent/>
+    </>
+  )
+}
+
+export default App
